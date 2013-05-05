d2c3aa613e13d50597477b6f19fccccd79bac46a
Phasmal <rob@mcauliffe.id.au>
2013-05-05 16:06:43 +1000

    removed previous version

--- config/initializers/session_store.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-RailstutorialOrg::Application.config.session_store :cookie_store, key: '_railstutorial.org_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# RailstutorialOrg::Application.config.session_store :active_record_store
