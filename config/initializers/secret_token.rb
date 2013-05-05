f76432cacf51b6f33760db92a151872b34712906
rob <none@none>
2013-05-05 17:42:17 +1000

    rails new + gemfile changes, ported from mercurial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '8d49d9f6313ded6455b9bf6cadde0e1ce656e0662b8505624c395cab392fc581e18f6419f21eeaf3e6501caaf4b2fd437bda1d7374d330e11e5f49552c1ef2d6'
